0b5d784cd7a841cb4b57febf40d4ba3f12b3f28b
Rohail Rafique <rohailrafique@gmail.com>
2021-09-09 17:15:58 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+
+function writeCards(people, event) {
+let messages = [];
+    for(let i = 0; i < people.length; i++) {messages.push(`Thank you, ${people[i]}, for the wonderful surprise gift!`)}
+    return messages;
+}
+
+
+function countDown(positiveNumber) {
+while(positiveNumber >= 0) {
+console.log(positiveNumber);
+positiveNumber--;
+}
+}
